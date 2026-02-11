7d985adc33d7aed3a72e68750e1d85e732f160bc
Michael2001papis <dvinka2@email.com>
2026-02-11 11:44:14 +0200

    0.0.1 תיקון קובץ ופרמטרים בביקורת

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  base: '/PTMAWDIFAPI/', // לתאימות GitHub Pages
   build: {
     outDir: 'dist',
     rollupOptions: {
